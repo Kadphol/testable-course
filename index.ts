eefff5681c288b96f83d0c4b0cef14370f2e979c
Kadphol <kadphol.len@outlook.co.th>
2025-08-23 22:13:50 +0700

    feat: finish app

--- index.ts
+++ index.ts
@@ -1,3 +1,15 @@
+import type { GameResult, Hand } from "./src/poker/hand";
+import { printResult } from "./src/printer";
+import { Reader } from "./src/reader";
+
 function main() {
-  console.log("test");
+  const reader = new Reader("./test_input/sample.json");
+  const hands = reader.read() as Hand[];
+
+  // eslint-disable-next-line no-console
+  console.log(
+    printResult(hands[0]?.compareHand(hands[1] as Hand) as GameResult),
+  );
 }
+
+main();
